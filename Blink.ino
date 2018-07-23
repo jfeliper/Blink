520cba3aa5594e816add3f604bc7c14a6ca54e09
Joaquin <joaquin.felipe@gmail.com>
2018-07-23 22:34:15 +0200

    Cambio de tiempo apagado

--- Blink.ino
+++ Blink.ino
@@ -23,6 +23,7 @@ version en github
   http://www.arduino.cc/en/Tutorial/Blink
 */
 
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
@@ -34,5 +35,5 @@ void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(100);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  delay(300);                       // wait for a second
 }
